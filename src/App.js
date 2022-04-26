66eaeb4cc2d499fdb96e6fc55153d5302e5f0476
DevLeeYG <hi33741@naver.com>
2022-04-26 16:38:43 +0900

    8

--- src/App.js
+++ src/App.js
@@ -6,17 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          a Edit <code>src/App.js</code> and save to reload.fdsf
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>8번쨰</p>
       </header>
     </div>
   );
