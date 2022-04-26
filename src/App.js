57ac72aea1d373e5958aef2d3f39d685f756cfd9
DevLeeYG <hi33741@naver.com>
2022-04-26 17:21:40 +0900

    revert k8

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         feature/yg
         <p>8번쨰입니다 아닐까요?</p>
+        <p>9번쨰입니다 아닐까요?</p>
       </header>
     </div>
   );
