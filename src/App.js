3eb3424a5e438291e8c4d9b886d3aac685a19187
DevLeeYG <hi33741@naver.com>
2022-04-26 16:43:01 +0900

    8번쨰입니다 아닐까요?

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>8번쨰입니다</p>
+        <p>8번쨰입니다. 아닙니다</p>
       </header>
     </div>
   );
