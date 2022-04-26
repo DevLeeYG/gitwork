da860b226a6ab7a17e3e30e3c499de1ad583aa04
DevLeeYG <hi33741@naver.com>
2022-04-26 17:26:25 +0900

    Revert "1"
    This reverts commit de4b5985c9870324ebf76ee6ff06b18d85d73b51.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import "./App.css";
-import logo from "./logo.svg";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        feature/yg
-        <p>8번쨰입니다 아닐까요?</p>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
     </div>
   );
